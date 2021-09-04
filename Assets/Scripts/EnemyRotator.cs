a784365bd087f805c274b200c2d40728c910cf07
zn-mehedi <zn.mehedi@gmail.com>
2021-09-04 22:14:24 +0600

    first commit

---
+++ Assets/Scripts/EnemyRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotator : MonoBehaviour
+{
+    public int rotationSpeed;
+    private Collider cd;  
+    void Start()
+    {
+        cd = GetComponent<Collider>();
+        cd.isTrigger = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(10, 10, 10), rotationSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Drawing;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    private Rigidbody rg;
+    private Vector3 moveMent;
+    private float moveHorizontal, moveVertical;
+    public int speed;
+    public Text countPoint;
+    public Text Wintext;
+    private int point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+        point = 0;
+        setCountPoint();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        moveMent = new Vector3(moveHorizontal, 0f, moveVertical);
+        rg.AddForce(moveMent * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            point++;
+            setCountPoint();
+        }
+    }
+    void setCountPoint()
+    {
+        countPoint.text = "Point: " + point.ToString();
+        if (point >= 8)
+        {
+            Wintext.text = "You Win ...!";
+        }
+    }
+
+}
